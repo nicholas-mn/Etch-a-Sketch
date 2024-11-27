aaa404bd85f0e66dc9300c7aab6e4e4baa7b5c9e
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2024-11-27 21:31:14 +0100

    create hovering effect on grid

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ function createGrid(amount) {
         const gridElement = document.createElement("div");
         gridElement.classList.add("gridElement");
         gridElement.style.flexBasis = 100 / amount + "%";
+
+        gridElement.addEventListener("mouseover", () => {
+            gridElement.style.backgroundColor = "black";
+        });
+
         gridDiv.appendChild(gridElement);
 
     }
